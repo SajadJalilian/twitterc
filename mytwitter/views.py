bb89910290baae31bc2472d8f1b9b2456d286a57
Sajad Jalilian <sajadjalilian88@gmail.com>
2020-05-13 01:52:30 +0430

    adding logic to views

--- mytwitter/views.py
+++ mytwitter/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, DeleteView
+from .models import Status
 
 
 def home(request):
@@ -12,16 +13,29 @@ def about(request):
 
 
 class StatusListView(ListView):
-    pass
+    model = Status
+    template_name = "mytwitter/home.html"
+    context_object_name = "statuses"
+    paginate_by = 5
 
 
 class StatusDeleteView(DeleteView):
-    pass
+    model = Status
+    success_url = "/"
+
+    def test_func(self):
+        status = self.get_object()
+        if self.request.user == status.author:
+            return True
+        return False
 
 
 class StatusDetailView(DetailView):
-    pass
+    model = Status
 
 
 class UserStatusListView(ListView):
-    pass
+    model = Status
+    template_name = "mytwitter/user_status.html"
+    context_object_name = "statuses"
+    paginate_by = 5
