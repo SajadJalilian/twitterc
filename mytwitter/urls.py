76d487d67d1400cda0b0cd39e8a079e743e61968
Sajad Jalilian <sajadjalilian88@gmail.com>
2020-05-14 18:23:40 +0430

    home route and user-status route are working

--- mytwitter/urls.py
+++ mytwitter/urls.py
@@ -11,8 +11,8 @@ from . import views
 
 urlpatterns = [
     path("", StatusListView.as_view(), name="mytwitter-home"),
-    path("status/<int:pk>/", StatusDetailView.as_view, name="status-detail"),
-    path("status/<int:pk>/delete/", StatusDeleteView.as_view, name="user-delete"),
-    path("user/<str:username>/", UserStatusListView.as_view, name="user-statuses"),
+    path("user/<str:username>/", UserStatusListView.as_view(), name="user-statuses"),
+    path("status/<int:pk>/", StatusDetailView.as_view(), name="status-detail"),
+    path("status/<int:pk>/delete/", StatusDeleteView.as_view(), name="user-delete"),
     path("about/", views.about, name="mytwitter-about"),
 ]
--- mytwitter/views.py
+++ mytwitter/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, DeleteView
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.views.generic import (
+    ListView,
+    DetailView,
+    DeleteView,
+    CreateView,
+    UpdateView,
+)
 from .models import Status
 
 
@@ -8,18 +16,53 @@ def home(request):
     return render(request, "mytwitter/home.html", context)
 
 
-def about(request):
-    return render(request, "mytwitter/about.html", {"title": "About"})
-
-
 class StatusListView(ListView):
     model = Status
     template_name = "mytwitter/home.html"
     context_object_name = "statuses"
-    paginate_by = 5
+    paginate_by = 10
 
 
-class StatusDeleteView(DeleteView):
+class UserStatusListView(ListView):
+    model = Status
+    template_name = "mytwitter/user_status.html"
+    context_object_name = "user_statuses"
+    paginate_by = 10
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get("username"))
+        return Status.objects.filter(author=user).order_by("-date_posted")
+
+
+class StatusDetailView(DetailView):
+    model = Status
+
+
+class StatusCreateView(LoginRequiredMixin, CreateView):
+    model = Status
+    fields = ["message"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class StatusUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Status
+    fields = ["message"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        status = self.get_object()
+        if self.request.user == status.author:
+            return True
+        return False
+
+
+class StatusDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Status
     success_url = "/"
 
@@ -30,12 +73,5 @@ class StatusDeleteView(DeleteView):
         return False
 
 
-class StatusDetailView(DetailView):
-    model = Status
-
-
-class UserStatusListView(ListView):
-    model = Status
-    template_name = "mytwitter/user_status.html"
-    context_object_name = "statuses"
-    paginate_by = 5
+def about(request):
+    return render(request, "mytwitter/about.html", {"title": "About"})
